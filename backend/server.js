bc606d23b459451525e54f700c3e623ad3b0a474
Tate Jenkins <tatejenkins113@gmail.com>
2023-02-03 11:59:10 -0800

    prodpath

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,15 @@ import cors from "cors"
 import names from "./api/names.route.js"
 
 const app = express()
+const path = require('path')
 
 app.use(cors())
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../frontend/build'))
+    app.use(express.static(path.join(__dirname,'frontend','build','index.html')))
 }
-//production
-app.use(express.static('frontend/build/index.html'))
+
 
 app.use("/api/v1/names", names)
 app.use("*", (req, res) => res.status(404).json({ error: "This isn't a valid page.}"}))
